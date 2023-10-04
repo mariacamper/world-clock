467f3b9ce4c5343ab1fbe9f34c746689293b53ce
mariacamper <m.campsite@yahoo.com>
2023-10-04 07:11:47 -0400

    Added the function for the select

--- javascript/index.js
+++ javascript/index.js
@@ -23,7 +23,29 @@ sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do, YYYY");
 sydneyTimeElement.innerHTML = sydneyTime.format("h:mm:ss:SSS [<small>]A[</small>]");
  
 }
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone); 
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city"<div>
+    <h2>${cityName}</h2>
+    <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+  </div>
+  <div class="time">${cityTime.format("h:mm:ss:SSS")} <small>${cityTime.format("A")}</small></div>
+</div>
+  `;
+}
+
 updateTime();
 setInterval(updateTime, 1);
 //SSS=milliseconds also 1=milliseconds. 1000=just every second=ss
+
+let citiesSelectElement = document.querySelector("#city");
+
+citiesSelectElement.addEventListener("change", updateCity);
+
+
 
